07744369153c47baedd657e71a82be43100d4ab2
Kamran Qadir <qadir0108@gmail.com>
2021-12-08 13:25:28 +0500

    added first WPF application

---
+++ WpfAppFirst/WpfAppFirst/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppFirst
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Check Login Username & Password
+            if (txtUserName.Text == "admin" && txtPassword.Text == "admin")
+            {
+                lblMessage.Content = "Welcome " + txtUserName.Text + "!";
+            }
+            else
+            {
+                MessageBox.Show("Please enter correct Username and Password");
+            }
+        }
+    }
+}
