1d81080bc2a161a0bcb373f69173d99dec50b467
Kamran Qadir <qadir0108@hotmail.com>
2022-11-25 10:15:37 +0500

    added QR Code project

---
+++ QRCode/QRCode/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace QRCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = new ZXing.QrCode.QrCodeEncodingOptions
+            {
+                Height = 200,
+                Width = 200,
+            };
+            Bitmap image = writer.Write("BSIT");
+            pictureBox1.Image = image;
+        }
+    }
+}
