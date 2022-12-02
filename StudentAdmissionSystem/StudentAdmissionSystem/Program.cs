566fc9bc98229303aac0e9edee07f79b84bf4291
Kamran Qadir <qadir0108@hotmail.com>
2022-12-02 11:55:48 +0500

    set first screen

--- StudentAdmissionSystem/StudentAdmissionSystem/Program.cs
+++ StudentAdmissionSystem/StudentAdmissionSystem/Program.cs
@@ -16,7 +16,7 @@ namespace StudentAdmissionSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmRegistration());
+            Application.Run(new frmProfile());
         }
     }
 }
