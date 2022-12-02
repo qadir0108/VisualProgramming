4fe2fa9a349f73984e6f6286378883424eb690a3
Kamran Qadir <qadir0108@hotmail.com>
2022-12-02 11:54:57 +0500

    added data project

---
+++ StudentAdmissionSystem/StudentAdmissionSystem.Data/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentAdmissionSystem.Data
+{
+    public class Student
+    {
+        public long CNIC { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ StudentAdmissionSystem/StudentAdmissionSystem.Data/StudentManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAdmissionSystem.Data
+{
+    public class StudentManager
+    {
+        static List<Student> students = new List<Student>();
+
+        public void Add(long _cnic, string _firstName, string _lastName)
+        {
+            Student student = new Student();
+            student.CNIC = _cnic;
+            student.FirstName = _firstName;
+            student.LastName = _lastName;
+            students.Add(student);
+        }
+
+        public List<Student> GetAll()
+        {
+            return students;
+            //{
+            //    Debug.WriteLine("CNIC : " + student.CNIC);
+            //    Debug.WriteLine("First : " + student.FirstName);
+            //    Debug.WriteLine("Last : " + student.LastName);
+            //}
+        }
+    }
+}
