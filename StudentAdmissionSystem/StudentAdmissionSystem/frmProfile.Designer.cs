f0bcd0a4de787bda4b9c99a7ceae3cd14c44f27e
Kamran Qadir <qadir0108@hotmail.com>
2022-12-02 11:58:23 +0500

    called student manger function on click

--- StudentAdmissionSystem/StudentAdmissionSystem/frmProfile.Designer.cs
+++ StudentAdmissionSystem/StudentAdmissionSystem/frmProfile.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.label4 = new System.Windows.Forms.Label();
@@ -40,7 +40,7 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtCNIC = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
@@ -54,6 +54,12 @@
             this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvStudents = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -66,12 +72,12 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Admission System";
             // 
-            // textBox1
+            // txtFirstName
             // 
-            this.textBox1.Location = new System.Drawing.Point(308, 103);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(206, 22);
-            this.textBox1.TabIndex = 1;
+            this.txtFirstName.Location = new System.Drawing.Point(308, 103);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(217, 22);
+            this.txtFirstName.TabIndex = 1;
             // 
             // label2
             // 
@@ -91,17 +97,17 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Last Name";
             // 
-            // textBox2
+            // txtLastName
             // 
-            this.textBox2.Location = new System.Drawing.Point(308, 153);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(217, 22);
-            this.textBox2.TabIndex = 3;
+            this.txtLastName.Location = new System.Drawing.Point(308, 153);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(217, 22);
+            this.txtLastName.TabIndex = 2;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(418, 219);
+            this.radioButton1.Location = new System.Drawing.Point(412, 431);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(74, 20);
             this.radioButton1.TabIndex = 5;
@@ -112,7 +118,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(308, 219);
+            this.radioButton2.Location = new System.Drawing.Point(302, 431);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(58, 20);
             this.radioButton2.TabIndex = 6;
@@ -123,7 +129,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(211, 219);
+            this.label4.Location = new System.Drawing.Point(216, 431);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(52, 16);
             this.label4.TabIndex = 7;
@@ -132,7 +138,7 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(550, 219);
+            this.radioButton3.Location = new System.Drawing.Point(544, 431);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(67, 20);
             this.radioButton3.TabIndex = 8;
@@ -142,15 +148,15 @@
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(314, 287);
+            this.dateTimePicker1.Location = new System.Drawing.Point(303, 466);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(217, 22);
             this.dateTimePicker1.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(221, 287);
+            this.label5.Location = new System.Drawing.Point(195, 471);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(79, 16);
             this.label5.TabIndex = 10;
@@ -159,23 +165,23 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(217, 348);
+            this.label6.Location = new System.Drawing.Point(245, 200);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(38, 16);
             this.label6.TabIndex = 12;
             this.label6.Text = "CNIC";
             // 
-            // textBox3
+            // txtCNIC
             // 
-            this.textBox3.Location = new System.Drawing.Point(314, 348);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(217, 22);
-            this.textBox3.TabIndex = 11;
+            this.txtCNIC.Location = new System.Drawing.Point(308, 200);
+            this.txtCNIC.Name = "txtCNIC";
+            this.txtCNIC.Size = new System.Drawing.Size(217, 22);
+            this.txtCNIC.TabIndex = 3;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(217, 401);
+            this.label7.Location = new System.Drawing.Point(177, 514);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(97, 16);
             this.label7.TabIndex = 14;
@@ -183,7 +189,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(314, 401);
+            this.textBox4.Location = new System.Drawing.Point(303, 514);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(217, 22);
             this.textBox4.TabIndex = 13;
@@ -191,7 +197,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(217, 455);
+            this.label8.Location = new System.Drawing.Point(216, 568);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(58, 16);
             this.label8.TabIndex = 16;
@@ -199,7 +205,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(314, 455);
+            this.textBox5.Location = new System.Drawing.Point(303, 568);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(217, 22);
             this.textBox5.TabIndex = 15;
@@ -211,15 +217,15 @@
             "Multan",
             "Bahwalpur",
             "Lodhran"});
-            this.comboBox1.Location = new System.Drawing.Point(314, 510);
+            this.comboBox1.Location = new System.Drawing.Point(303, 623);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(211, 24);
+            this.comboBox1.Size = new System.Drawing.Size(217, 24);
             this.comboBox1.TabIndex = 17;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(221, 518);
+            this.label9.Location = new System.Drawing.Point(245, 626);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(29, 16);
             this.label9.TabIndex = 18;
@@ -228,7 +234,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(227, 575);
+            this.label10.Location = new System.Drawing.Point(214, 675);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(60, 16);
             this.label10.TabIndex = 20;
@@ -241,16 +247,16 @@
             "Multan",
             "Bahwalpur",
             "Lodhran"});
-            this.comboBox2.Location = new System.Drawing.Point(320, 567);
+            this.comboBox2.Location = new System.Drawing.Point(303, 667);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(211, 24);
+            this.comboBox2.Size = new System.Drawing.Size(217, 24);
             this.comboBox2.TabIndex = 19;
             // 
             // label12
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(142, 621);
+            this.label12.Location = new System.Drawing.Point(131, 721);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(118, 29);
             this.label12.TabIndex = 49;
@@ -259,7 +265,7 @@
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(513, 670);
+            this.checkBox4.Location = new System.Drawing.Point(502, 770);
             this.checkBox4.Name = "checkBox4";
             this.checkBox4.Size = new System.Drawing.Size(153, 20);
             this.checkBox4.TabIndex = 48;
@@ -269,7 +275,7 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(388, 670);
+            this.checkBox3.Location = new System.Drawing.Point(377, 770);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(90, 20);
             this.checkBox3.TabIndex = 47;
@@ -279,7 +285,7 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(270, 670);
+            this.checkBox2.Location = new System.Drawing.Point(259, 770);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(73, 20);
             this.checkBox2.TabIndex = 46;
@@ -289,18 +295,73 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(169, 670);
+            this.checkBox1.Location = new System.Drawing.Point(158, 770);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(41, 20);
             this.checkBox1.TabIndex = 45;
             this.checkBox1.Text = "IT";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(555, 168);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 50;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(663, 168);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 51;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(663, 197);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 53;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(555, 197);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 52;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvStudents
+            // 
+            this.dgvStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudents.Location = new System.Drawing.Point(136, 256);
+            this.dgvStudents.Name = "dgvStudents";
+            this.dgvStudents.RowHeadersWidth = 51;
+            this.dgvStudents.RowTemplate.Height = 24;
+            this.dgvStudents.Size = new System.Drawing.Size(602, 134);
+            this.dgvStudents.TabIndex = 54;
+            // 
             // frmProfile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(827, 727);
+            this.ClientSize = new System.Drawing.Size(827, 815);
+            this.Controls.Add(this.dgvStudents);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.checkBox3);
@@ -315,7 +376,7 @@
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtCNIC);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.radioButton3);
@@ -323,12 +384,15 @@
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtLastName);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtFirstName);
             this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmProfile";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudents)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -337,10 +401,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtFirstName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.Label label4;
@@ -348,7 +412,7 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtCNIC;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label8;
@@ -362,6 +426,11 @@
         private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvStudents;
     }
 }
 
--- StudentAdmissionSystem/StudentAdmissionSystem/frmProfile.cs
+++ StudentAdmissionSystem/StudentAdmissionSystem/frmProfile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentAdmissionSystem.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,73 @@ namespace StudentAdmissionSystem
         public frmProfile()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            StudentManager manager = new StudentManager();
+            try
+            {
+                long cnic = long.Parse(txtCNIC.Text);
+                manager.Add(cnic, txtFirstName.Text, txtLastName.Text);
+                MessageBox.Show("New Student Added");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            StudentManager manager = new StudentManager();
+            try
+            {
+                long cnic = long.Parse(txtCNIC.Text);
+                Student found = manager.Search(cnic);
+
+                if (found == null)
+                    MessageBox.Show("Not Found.");
+                else
+                {
+                    txtFirstName.Text = found.FirstName;
+                    txtLastName.Text = found.LastName;
+                    MessageBox.Show("Stduent Found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            StudentManager manager = new StudentManager();
+            try
+            {
+                long cnic = long.Parse(txtCNIC.Text);
+                bool result = manager.Delete(cnic);
+                if (result == true)
+                    MessageBox.Show("Delete.");
+                else
+                    MessageBox.Show("Cannot Delete");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            StudentManager manager = new StudentManager();
+            List<Student> students = manager.GetAll();
+
+            BindingSource source = new BindingSource();
+            source.DataSource = students;
+
+            dgvStudents.DataSource = source;
         }
     }
 }
